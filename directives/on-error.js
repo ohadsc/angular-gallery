658a60d96325731747beed5e6ed186d63994a00d
ohadsc <ohad.schachtel@gmail.com>
2016-05-16 15:35:09 +0300

    on error fix

--- directives/on-error.js
+++ directives/on-error.js
@@ -1,13 +1,10 @@
-app.directive('onError', ['$http', function($http) {
+app.directive('onError', function() {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
-            attrs.$observe('ngSrc', function(ngSrc) {
-                $http.get(ngSrc).success(function(){
-                }).error(function(){
-                    element.attr('src', 'http://rhodium.co.il/wp-content/uploads/2013/05/logo_yotpo.png');
-                });
+            element.bind('error', function() {
+                element.attr('src', 'http://www.leapgo.com/wp-content/uploads/2014/05/yotpo-logo.png');
             });
         }
     };
-}]);
+});
